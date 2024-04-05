0b6146909723cb842953619082b8fd1f269e4d9d
Artyom Smirnov <artyomsmirnov1111@gmail.com>
2024-04-05 14:24:19 +0300

    add login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ import { login, logout, signup } from "../controllers/authController.js"
 
 const router = express.Router()
 
-router.get("/login", login)
+router.post("/login", login)
 
 router.post("/signup", signup)
 
