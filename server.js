fd9adf981999032008bef802bc523e75b36317e0
Artyom Smirnov <artyomsmirnov1111@gmail.com>
2024-04-15 15:09:03 +0300

    add websockets

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
-import dotenv from "dotenv";
-import express from "express";
+import dotenv from "dotenv"
+import express from "express"
 
-import authRoutes from "./routes/authRoutes.js";
-import messageRoutes from "./routes/messageRoutes.js";
+import authRoutes from "./routes/authRoutes.js"
+import messageRoutes from "./routes/messageRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
 
-import connectToMongo from "./db/connectToMongo.js";
+import connectToMongo from "./db/connectToMongo.js"
+import { app, server } from "./socket/socket.js"
 
-import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser"
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-dotenv.config();
-
-const app = express();
+dotenv.config()
 
 app.use(express.json())
 app.use(cookieParser())
 
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
-app.listen(PORT, () => {
-  connectToMongo();
-  console.log(`server running on ${PORT}`);
-});
+server.listen(PORT, () => {
+	connectToMongo()
+	console.log(`server running on ${PORT}`)
+})
